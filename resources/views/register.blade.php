faa757792d45cabf77352290ec06c4b7f1a42cc9
PratamaIwang <pratamaiwang16@gmail.com>
2021-12-13 23:10:37 +0700

    Login Register

---
+++ resources/views/register.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </head>
+    <body>
+        <!-- Navigation-->
+    <section class="vh-100">
+        <div class="container-fluid h-custom">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-md-9 col-lg-6 col-xl-5">
+                <img src="{{asset('/img/food_login.png')}}" class="img-fluid"
+                alt="Sample image">
+            </div>
+            <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                <form>
+                <h1>Daftar dulu Dong hehe</h1>
+                <br>
+                <div class="form-outline mb-4">
+                    <input type="text" id="input-nama" class="form-control form-control-lg"
+                    placeholder="Masukin Email Kamu" />
+                    <label class="form-label" for="form3Example3">Nama Kamu</label>
+                </div>
+                <!-- Email input -->
+                <div class="form-outline mb-4">
+                    <input type="email" id="input-nama" class="form-control form-control-lg"
+                    placeholder="Masukin Email Kamu" />
+                    <label class="form-label" for="form3Example3">Email</label>
+                </div>
+
+                <!-- Password input -->
+                <div class="form-outline mb-3">
+                    <input type="password" id="input-nama" class="form-control form-control-lg"
+                    placeholder="Isi Juga Passwordnya disini" />
+                    <label class="form-label" for="form3Example4">Password</label>
+                </div>
+
+                <div class="text-center text-lg-start mt-4 pt-2">
+                    <button type="button" class="btn btn-primary btn-lg"
+                    style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
+                    <p class="small fw-bold mt-2 pt-1 mb-0">Udah Punya Akun? <a href="login"
+                        class="link-danger">Login yuk</a></p>
+                </div>
+
+                </form>
+            </div>
+            </div>
+        </div>
+       
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
+</html>
